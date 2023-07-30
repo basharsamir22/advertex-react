7d8e7c8362564a82d89f7ff83b5a3a5aff44249d
bashar samir <basharsamir@MacBook.local>
2023-07-30 14:45:12 +0300

    title remive

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../css/style.css"
 import BanarHead from "../components/Utility/BanarHead";
-import Title from "../components/Utility/Title";
 import StartHead from "../components/ContactUs/StartHead";
 import Map from "../components/ContactUs/Map";
 import Form from "../components/ContactUs/Form";
